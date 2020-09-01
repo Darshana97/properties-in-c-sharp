3040759463119d5b4e4dc26b044ca5c30273cb27
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-09-01 14:16:49 +0530

    properties

--- properties/Program.cs
+++ properties/Program.cs
@@ -9,13 +9,22 @@ namespace properties
 
     class Students
     {
-        public int _Id;
-        public string _FirstName;
-        public string _LastName;
+        private int _Id;
+        private string _FirstName;
+        private string _LastName;
 
-        public void SetId()
+        public void SetId(int Id)
         {
+            if (Id <= 0)
+            {
+                throw new Exception("Student id cannot be negative");
+            }
+            this._Id = Id;
+        }
 
+        public int GetId()
+        {
+            return this._Id;
         }
 
 
@@ -27,12 +36,15 @@ namespace properties
         static void Main(string[] args)
         {
             Students S1 = new Students();
-            S1._Id = 100;
+            S1.SetId(-100);
+
+            
+            /*S1._Id = 100;
             S1._FirstName = "Darshana";
             S1._LastName = "Senevirathna";
 
             Console.WriteLine("My id is = {0}, i'm {1} {2}", S1._Id,S1._FirstName,S1._LastName);
-            Console.ReadLine();
+            Console.ReadLine();*/
 
         }
     }
